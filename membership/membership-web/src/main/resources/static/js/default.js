acdf74b0e22806561e48b066e8a8e62b04d5e13f
Tany <2971183859@qq.com>
2021-03-02 16:12:11 +0800

    代码微调3

---
+++ membership/membership-web/src/main/resources/static/js/default.js
@@ -0,0 +1,54 @@
+const base={
+    baseUrl:'https://localhost:8081',
+    chengpin:'/api/blueberrypai/getChengpinDetails.php',
+    login:'/api/blueberrypai/login.php',
+    music:"/sxtstu/music/baidu/list.php",
+    login:'/api/login',
+    list:'/api/list'
+}
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.common['tokenid'] = sessionStorage.getItem("tokenid");
+axios.defaults.baseURL = base.baseUrl;
+axios.defaults.timeout = 10000;
+axios.defaults.response.use(
+    // 响应包含以下信息data,status,statusText,headers,config
+    (res) => res.status === 200 ? Promise.resolve(res) : Promise.reject(res),
+    (err) => {
+        console.log(err)
+        const { response } = err;
+        // console.log(response)
+        if (response) {
+            errorHandle(response.status, response.data);
+            return Promise.reject(response);
+        } else {
+            console.log('请求失败')
+        }
+    }
+);
+const errorHandle = (status, other) => {
+    switch (status) {
+        case 400:
+            console.log("信息校验失败");
+            break;
+        case 401:
+            console.log("认证失败");
+            break;
+        case 403:
+            console.log("token校验失败");
+            break;
+        case 404:
+            console.log("请求的资源不存在");
+            break;
+        case 500:
+            console.log("500：访问服务失败");
+            break;
+        case 503:
+            console.log("503：服务不可用");
+            break;
+        default:
+            console.log(other);
+            break;
+
+    }
+}
